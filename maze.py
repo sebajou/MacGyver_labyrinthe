32f6606e94f9ab8f6ad1ccde95df85995cdd2baa
sebajou <sebajou@gmail.com>
2019-11-13 16:42:39 +0100

    little improve and add of comment

--- maze.py
+++ maze.py
@@ -2,44 +2,58 @@
 # -*-coding:Utf-8 -*
 
 """
-This program are a play where you drive Mac Gyver out of a maze. 
+This is a game where you drive Mac Gyver out of a maze. 
 """
 
 ##############################################################################################
-#               authors: Sébastien Jourdan  
-#               title: Mac Gyver maze
+#=============================================================================================
+#                           title: Mac Gyver maze
+#=============================================================================================
+#                           author: Sébastien Jourdan 
+#                                    /-\
+#                                   c° °>
+#                                    \O/
+#                           email: sebajou@gmail.com
+#                           society: None
+#                           Initial date: November 2019
+#                           version: v1
+#                           History: None
+#                           Comment: This program is a game where you drive Mac Gyver 
+# out of a maze. 
 ##############################################################################################
 
 #load file maze_plan.txt in table of table (rack)
 maze = open("maze_plan.txt", "r")
-
 maze = maze.read()
 maze = maze.split("\n")
 rack = []
 
+#usefull function
+def print_maze(rack):
+    """Pretty print of maze"""
+    for j in range(15):
+        print("\n", end='')
+        for i in range(15):
+            print(rack[j][i], end=' ')
+
+#setting up table of table wich contain the maze. 
 for line in maze:
     list_line = []
     for caractere in line: 
         list_line.append(caractere)
     rack.append(list_line)
 
-rack
-#maze.close()
-
 #load Mac Gyver (X) at the entrance of maze, rack[y][x]
 y=1
 x=0
 rack[y][x] = "X"
-for j in range(14):
-    print("\n", end='')
-    for i in range(14):
-        print(rack[j][i], end='')
+print_maze(rack)
+
 #Alow to move Mac Gyver and control that Mac Gyver don't go trough the wall
-
 while not (y==13 and x==14):
-
+    #input keyboard
     direction = input("Wich direction you want to go? (upward u, downward n, left h, right j): ")
-    
+    #move upward    
     if direction == "u": 
         rack[y][x] = "+"
         if (y-1<0 or rack[y-1][x]=="M"):
@@ -47,7 +61,7 @@ while not (y==13 and x==14):
         else:
             y=y-1
             rack[y][x] = "X"
-        #move upward
+    #move downward
     elif direction == "n":
         rack[y][x] = "+"
         if (y+1>14 or rack[y+1][x]=="M"):
@@ -55,7 +69,7 @@ while not (y==13 and x==14):
         else:
             y=y+1
             rack[y][x] = "X"
-        #move downward
+    #move left
     elif direction == "h":
         rack[y][x] = "+"
         if (x-1<0 or rack[y][x-1]=="M"):
@@ -63,7 +77,7 @@ while not (y==13 and x==14):
         else:
             x=x-1
             rack[y][x] = "X"
-        #move left
+    #move right
     elif direction == "j":
         rack[y][x] = "+"
         if (x+1>14 or rack[y][x+1]=="M"):
@@ -71,20 +85,15 @@ while not (y==13 and x==14):
         else:
             x=x+1
             rack[y][x] = "X"
-        #move right
+    #unapropriate keyboard
     else:
         pass
-    for j in range(14):
-        print("\n", end='')
-        for i in range(14):
-            print(rack[j][i], end='')
+    #Print maze
+    print_maze(rack)
             
 
-#for i, carte in enumerate(liste_cartes):
-#		print("  {} - {}".format(i + 1, carte.nom))
-#print(a, end='')
-
+#In case of victory
 if (y==13 and x==14):
-    print(rack)
+    print_maze(rack)
     for i in range(0,24):
                 print("You win !")
